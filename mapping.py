2028ab831acd7abb2e92ea3025066aaf88331d0d
haval95 <havalrezan22@gmail.com>
2023-04-05 00:18:24 +0300

    all done, with layers turning on and off

--- mapping.py
+++ mapping.py
@@ -8,7 +8,7 @@ country_map = folium.Map(location=[33.3152, 44.3661], zoom_start=6)
 # folium.Marker(location=[33.3152, 44.3661], popup='Iraq').add_to(country_map)
 # folium.CircleMarker(location=[33.3152, 44.3661],radius=5, fill_color="PuBu", tooltip="haval").add_to(country_map)
 
-fg = folium.FeatureGroup(name="my map")
+fg = folium.FeatureGroup(name="Volcanoes")
 
 volcanoes = pd.read_csv("volcanoes.csv")
 
@@ -27,11 +27,15 @@ for cordinate in cordinates:
     fg.add_child(folium.CircleMarker(location=cordinate[0:2],tooltip=cordinate[2],popup=cordinate[3] ,radius=6, fill_color = get_color_based_on_elv(cordinate[3]),
                                      color=get_color_based_on_elv(cordinate[3]), fill_opacity=0.7)) 
     
-fg.add_child(folium.GeoJson(data=open('world.json', "r", encoding="utf-8-sig").read(),
+fgp = folium.FeatureGroup(name="population")
+fgp.add_child(folium.GeoJson(data=open('world.json', "r", encoding="utf-8-sig").read(),
                             style_function= lambda x: {"fillColor": "green" if x['properties']['POP2005'] < 10000000
                                                        else  "orange" if 10000000 <= x['properties']['POP2005'] and 20000000 > x['properties']['POP2005'] else "red" }))
+
+
 country_map.add_child(fg)
- 
+country_map.add_child(fgp)
+country_map.add_child(folium.LayerControl())
 
 # Display the map
 country_map.save("map1.html")
