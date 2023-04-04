a038e516ba30e6bbef4e41ed5010fbc5788bc043
haval95 <havalrezan22@gmail.com>
2023-04-04 21:25:23 +0300

    changing the markers to circle markers

--- mapping.py
+++ mapping.py
@@ -10,21 +10,24 @@ country_map = folium.Map(location=[33.3152, 44.3661], zoom_start=6)
 
 fg = folium.FeatureGroup(name="my map")
 
-    
 volcanoes = pd.read_csv("volcanoes.csv")
-
 
 cordinates = list(map(list, zip(volcanoes["Latitude"], volcanoes["Longitude"], volcanoes["Name"], volcanoes["Elev"])))
 
+def get_color_based_on_elv(elv) -> str:
+    elv= int(elv.replace("m","").replace(",", ""))
+    
+    if elv >=  2000:
+        return "red"
+    elif elv >= 500:
+        return "green"
+    return "blue"
+
 for cordinate in cordinates:
-    fg.add_child(folium.Marker(location=cordinate[0:2],tooltip=cordinate[2],popup=cordinate[3] , icon= folium.Icon(color="red"))) 
-  
-    
-    
-    
-    
+    fg.add_child(folium.CircleMarker(location=cordinate[0:2],tooltip=cordinate[2],popup=cordinate[3] ,radius=6, fill_color = get_color_based_on_elv(cordinate[3]), color=get_color_based_on_elv(cordinate[3]), fill_opacity=0.7)) 
 country_map.add_child(fg)
  
+
 # Display the map
 country_map.save("map1.html")
 
